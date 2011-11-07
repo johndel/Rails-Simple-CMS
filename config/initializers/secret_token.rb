18806f2fe16f18d577b2a326da367d2df938f5b4
John Deliyiannis <istoselidas@gmail.com>
2011-11-08 00:30:22 +0200

    My very first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gscms::Application.config.secret_token = '435b9a370333deb658f4a4eca84192d63b954b3221297411614364a7a028df2afd3df8aae9ffe608336aa8c208548cdc2e65dc1e000ab99e3e5ed6fd3900a4b6'
