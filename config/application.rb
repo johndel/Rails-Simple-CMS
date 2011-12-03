13f06b3d60f766c24e78bdb294619d158b3668b1
istoselidas <istoselidas@gmail.com>
2011-12-03 12:36:25 +0200

    Devise installed, User model generated. Happiness.

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,8 @@ module Gscms
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Also for Heroku Deployment => http://guides.rubyonrails.org/asset_pipeline.html#precompiling-assets
+    config.assets.initialize_on_precompile = false
   end
 end
