2c600ab5e74d0077314f40b052ca30d67cb1c374
Gil Gomes <gilgomesp@gmail.com>
2013-01-07 23:02:18 -0200

    removing duplicate permalink length validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -10,15 +10,11 @@ class Page < ActiveRecord::Base
   has_many :page_menu_mappings, :dependent => :delete_all
   has_many :menus, :through => :page_menu_mappings, :uniq => true
   
-  validates :name, :presence => true 
-  validates :name, :uniqueness => true 
-  validates :name, :length => { :maximum => 250 }
-  validates :permalink, :presence => true 
-  validates :permalink, :uniqueness => true 
-  validates :permalink, :length => { :maximum => 250 }
-  validates :title, :length => { :maximum => 250 }
+  validates :name, :permalink, :presence => true 
+  validates :name, :permalink, :uniqueness => true 
+  validates :name, :length => { :maximum => 250 } 
+  validates :permalink, :title, :length => { :maximum => 250 }
   validates :meta_description, :length => { :maximum => 250 }
-  validates :permalink, :length => { :maximum => 250 }
 
   def self.first_page
 	homepage_id = Setting.where(:meta_key => "homepage").first.meta_value
